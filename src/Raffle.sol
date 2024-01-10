27f51a5e05f9b8154e8bfa93981efe44f58f8e71
Peter Hind <peter.hind47@gmail.com>
2024-01-11 09:02:34 +1300

    add IncorrectEntranceFee error

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,13 +10,19 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    error Raffle__IncorrectEntranceFee();
+
     uint256 private i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__IncorrectEntranceFee();
+        }
+    }
 
     function pickWinner() public {}
 
